3168c93ab9014719556d2dafd80d01fcb64d61f1
Thuan <thuantoppo@gmail.com>
2025-12-06 22:49:45 +0700

    đăng ký Dependency Injection

--- DaiLy_Agri_Supply_Chain/Program.cs
+++ DaiLy_Agri_Supply_Chain/Program.cs
@@ -1,15 +1,29 @@
-﻿using DailyAgriSupplyChain.DAL.Helper;
-using DailyAgriSupplyChain.DAL.Helper.Interfaces;
+﻿using DailyAgriSupplyChain.DAL.Helper.Interfaces;
+using DailyAgriSupplyChain.DAL.Helper;
 using DailyAgriSupplyChain.DAL.Interfaces;
-using DailyAgriSupplyChain.DAL; // Giả sử DonHangDaiLyRepository nằm ở đây
-//using DailyAgriSupplyChain.BLL.Interfaces;
-//using DailyAgriSupplyChain.BLL.Services;
+using DailyAgriSupplyChain.DAL;
+using DailyAgriSupplyChain.BLL.Interfaces;
+using DailyAgriSupplyChain.BLL;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// ĐĂNG KÝ DEPENDENCY INJECTION CHO 3 LỚP 
+
+// 1. Đăng ký Database Helper (DAL Utility)
+builder.Services.AddScoped<IDatabaseHelper, DatabaseHelper>();
+
+// 2. Đăng ký Repository (DAL Contract)
+builder.Services.AddScoped<IDonHangDaiLyRepository, DonHangDaiLyRepository>();
+
+// 3. Đăng ký Business/Service (BLL Contract)
+builder.Services.AddScoped<IDonHangDaiLyBusiness, DonHangDaiLyBusiness>();
+
+
+// CẤU HÌNH API MẶC ĐỊNH
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
